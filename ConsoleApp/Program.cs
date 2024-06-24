cd315515c6b307fe915ac796f29123a90fcde297
Oleksandr Yesipov <oleksandr.yesipov@avanquest.com>
2024-06-24 15:08:56 +0300

    added DependencyInjection

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,7 +1,36 @@
 ﻿using ConsoleApp.Tasks;
+using Microsoft.Extensions.DependencyInjection;
 
-var task1 = new ThreadBackendTask();
-await task1.RunAsync();
+var serviceCollection = new ServiceCollection();
+ConfigureServices(serviceCollection);
 
-var task2 = new ExpressionBackendTask();
-await task2.RunAsync();
+var serviceProvider = serviceCollection.BuildServiceProvider();
+
+var app = serviceProvider.GetRequiredService<App>();
+await app.RunAsync();
+
+
+static void ConfigureServices(IServiceCollection services)
+{
+    services.AddTransient<IBackendTask,ThreadBackendTask>();
+    services.AddTransient<IBackendTask,ExpressionBackendTask>();
+    services.AddTransient<App>();
+}
+
+public class App
+{
+    private readonly IBackendTask _task1;
+    private readonly IBackendTask _task2;
+
+    public App(IBackendTask task1, IBackendTask task2)
+    {
+        _task1 = task1;
+        _task2 = task2;
+    }
+
+    public async Task RunAsync()
+    {
+        await _task1.RunAsync();
+        await _task2.RunAsync();
+    }
+}
